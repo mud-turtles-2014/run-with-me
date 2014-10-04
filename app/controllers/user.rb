3355452e770c72437f51e0fd344695a85aecac49
Alex Ung <alexung.ucsb@gmail.com>
2014-10-04 17:39:22 -0400

    login

---
+++ app/controllers/user.rb
@@ -0,0 +1,47 @@
+enable :sessions
+
+get '/session' do
+  erb :'users/login'
+end
+
+post '/session' do
+  user = User.authenticate(params[:username], params[:password])
+
+  if user == nil || user == false
+    session[:error] = "Login Failed"
+    redirect "/session"
+  else
+    session[:user_id] = user.id
+    redirect "/user/#{current_user.id}"
+  end
+end
+
+get '/user' do
+  erb :'users/create_user'
+end
+
+get '/user/:user_id' do
+  @runs = Run.all
+  erb :'run_homepage'
+end
+
+post '/user' do
+  user = User.new(params[:user])
+  if user.valid?
+    user.save
+    redirect '/session'
+  else
+    redirect '/user'
+  end
+end
+
+delete '/session' do
+  session.clear
+  erb :index
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
+
